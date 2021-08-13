0e58e6ce830c466fdc49fe39266693739cd2e843
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-14 00:33:32 +0530

    refactor: database file is added in separate file

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const dbConfig = require("./database.config");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(dbConfig.url, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
