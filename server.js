6b6cfd178155db72048e9c24ca90eeab6e00457e
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-14 00:36:10 +0530

    refactor: database connection is added in server file

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
+require("./config/connection");
 const express = require("express");
 const app = express();
-const dbConfig = require("./config/database.config.js");
-const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-mongoose.Promise = global.Promise;
-
-mongoose
-  .connect(dbConfig.url, { useNewUrlParser: true })
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch((err) => {
-    console.log("Could not connect to the database. Exiting now...", err);
-    process.exit();
-  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Emploee Payroll pages" });
