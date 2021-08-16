dae9d7d0f1b510e0ad533fee7647938821e3fe44
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-16 08:53:30 +0530

    refactor: added middleware of auth ti valid the token

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,8 @@
 const user = require("../controller/user");
 const userValidation = require("../util/userValidation");
+const auth = require("../util/auth");
+
 module.exports = (app) => {
   app.post("/register", userValidation, user.register);
-  app.post("/login", user.login);
+  app.post("/login", auth, user.login);
 };
