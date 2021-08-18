a4375b4fe5448b827aad608acdbd7d6647a85e97
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-18 07:51:44 +0530

    refactor: remove auth functionality

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,7 @@
 const user = require("../controller/user");
 const userValidation = require("../util/userValidation");
-const auth = require("../util/auth");
 
 module.exports = (app) => {
   app.post("/register", userValidation, user.register);
-  app.post("/login", auth, user.login);
+  app.post("/login", user.login);
 };
