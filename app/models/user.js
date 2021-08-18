468c04310a6c1485e807196dd92be0b64595b72a
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-18 07:52:21 +0530

    refactor:corrected the jwt token code

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const user = require("../service/user");
+const auth = require("../util/helper");
 
 const userSchema = mongoose.Schema({
   fName: { type: String, required: true },
@@ -9,61 +8,43 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
-  token: { type: String },
 });
 
-const userDbData = mongoose.model("user", userSchema);
+const Userdb = mongoose.model("user", userSchema);
 
 class UserModel {
   createDetails = (userDetails, saveUserData) => {
-    const token = jwt.sign(
-      { email: userDetails.email },
-      process.env.TOKEN_KEY,
-      { expiresIn: "60s" }
-    );
-    const newUser = new userDbData({
+    const newUser = new Userdb({
       fName: userDetails.fName,
       lName: userDetails.lName,
       email: userDetails.email,
       password: userDetails.password,
       confirmPassword: userDetails.confirmPassword,
-      token: token,
     });
     newUser.save((error, data) => {
       return error ? saveUserData(error, null) : saveUserData(null, data);
     });
-
-    //save user token
-    userDetails.token = token;
   };
 
   loginUser = async (loginData, authenticateUser) => {
     const passwordEnteredByUser = loginData.password;
-    const hash = await userDbData.findOne({ email: loginData.email });
+    const hash = await Userdb.findOne({ email: loginData.email });
     bcrypt.compare(passwordEnteredByUser, hash.password, (err, isMatch) => {
       if (err) {
         throw err;
       } else if (!isMatch) {
         return authenticateUser("Password doesn't match!", null);
       } else {
-        userDbData.findOne({ email: loginData.email }, (error, data) => {
+        Userdb.findOne({ email: loginData.email }, (error, data) => {
           if (error) {
             return authenticateUser(error, null);
-          } else if (!data) {
-            return authenticateUser("Invalid User Credentials", null);
           }
 
-          const token = jwt.sign(
-            { email: loginData.email },
-            process.env.TOKEN_KEY,
-            {
-              expiresIn: "60s",
-            }
-          );
-
-          // save user token
-          loginData.token = token;
-          return authenticateUser(null, data);
+          if (!data) {
+            return authenticateUser("Invalid User Credentials", null);
+          }
+          const token = auth.generateToken(data);
+          return authenticateUser(null, token);
         });
       }
     });
