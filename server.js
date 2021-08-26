3c6df52847ac2a1a0133aca84fc177e3311068b4
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-27 00:19:36 +0530

    feat: added swagger ui and setup the swagger document

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("./config/connection");
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger/swagger.json');
 const express = require("express");
 const app = express();
 
@@ -15,5 +17,7 @@ require("./app/routes/routes")(app);
 var server = app.listen(process.env.PORT, () => {
   console.log("Server is listening on port 3000");
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = server;
