c3cb55ef513a7e8d3113fbaced9618be6f2444ed
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-31 00:35:26 +0530

    feat: add route to create and get employees

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,12 @@
 const user = require("../controller/user");
+const employee = require("../controller/employee");
 const userValidation = require("../util/userValidation");
 
 module.exports = (app) => {
   app.post("/register", userValidation, user.register);
   app.post("/login", user.login);
+
+  // employee CRUD api
+  app.post('/createEmployee', employee.saveEmployee);
+  app.get('/getEmployees', employee.getAllEmployee);
 };
