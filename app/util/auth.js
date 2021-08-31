af1ea44a976cc8ad6b03c8711c286a63cf94fd96
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-01 00:46:28 +0530

    refactor: updated the token class

--- app/util/auth.js
+++ app/util/auth.js
@@ -1,22 +1,28 @@
+/**
+ * @description function to verify the token.
+ * @returns 
+ */
+
 const jwt = require("jsonwebtoken");
 
 const config = process.env;
 
-const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+class Auth {
+   verifyToken = (req, res, next) => {
+    const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.headers.authorization;
 
-  if (!token) {
-    return res.status(403).send("A token is required for authenication");
-  }
+    if (!token) {
+      return res.status(403).send("A token is required for authenication");
+    }
 
-  try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
-};
+    try {
+      var removedBearerFromHeaderToken = token.replace("Bearer",'').trim();
+      const decoded = jwt.verify(removedBearerFromHeaderToken, config.TOKEN_KEY);
+    } catch (err) {
+      return res.status(401).send("Invalid Token");
+    }
+    return next();
+  };
+}
 
-module.exports = verifyToken;
+module.exports = new Auth();
