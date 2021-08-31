66dbd94648ec02daa9d8ccb2c0d1304ae4609c02
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-01 00:48:20 +0530

    feat: added token verification code

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,15 +1,17 @@
 const user = require("../controller/user");
 const employee = require("../controller/employee");
 const userValidation = require("../util/userValidation");
+const auth = require('../util/auth');
 
 module.exports = (app) => {
   app.post("/register", userValidation, user.register);
   app.post("/login", user.login);
 
   // employee CRUD api
-  app.post('/createEmployee', employee.saveEmployee);
-  app.get('/getEmployees', employee.getAllEmployee);
-  app.get('/getEmployeeById/:employeeId', employee.getEmployeeById);
-  app.put('/updateEmployeeDetail/:employeeId', employee.updateEmployeeDetail);
-  app.delete('/deleteEmployeeById/:employeeId', employee.deleteEmployeeById);
+  console.log(auth);
+  app.post('/createEmployee', auth.verifyToken, employee.saveEmployee);
+  app.get('/getEmployees', auth.verifyToken, employee.getAllEmployee);
+  app.get('/getEmployeeById/:employeeId', auth.verifyToken, employee.getEmployeeById);
+  app.put('/updateEmployeeDetail/:employeeId', auth.verifyToken, employee.updateEmployeeDetail);
+  app.delete('/deleteEmployeeById/:employeeId', auth.verifyToken, employee.deleteEmployeeById);
 };
