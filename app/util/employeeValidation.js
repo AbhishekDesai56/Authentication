7464b35b69d3cff314157e0e1270044ddd2801d9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-09-06 01:46:02 +0530

    feat: added employee validation file

---
+++ app/util/employeeValidation.js
@@ -0,0 +1,31 @@
+const joi = require("joi");
+
+const validation = joi.object({
+  name: joi.string().pattern(new RegExp("^[A-Z]{1}[a-z]{2,}$")).required(),
+  gender: joi.any().valid("Male", "Female").error(() => 'Gender should be Male (or) Female'),
+  department: joi.array().items(joi.string()),
+  salary: joi.string().required(),
+  startDate: joi.string().required(),
+});
+
+const employeeValidation = (req, res, next) => {
+  const empData = {
+    name: req.body.name,
+    gender: req.body.gender,
+    department: req.body.department,
+    salary: req.body.salary,
+    startDate: req.body.startDate,
+  };
+
+  const { error } = validation.validate(empData);
+  if (error) {
+    return res.status(406).json({
+      success: true,
+      message: "Error in User Data",
+      error,
+    });
+  }
+  next();
+};
+
+module.exports = employeeValidation;
